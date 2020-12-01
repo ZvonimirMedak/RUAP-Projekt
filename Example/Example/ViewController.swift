546410199d19a38e961656f0c91dc6b396e3251e
Zvonimir Medak <zvonimir.medak@hotmail.com>
2020-12-01 18:29:27 +0100

    added string value to ViewController

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Zvonimir Medak on 01.12.2020..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let string = "Pozdrav ja sam Zvonimir Medak"
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
+
